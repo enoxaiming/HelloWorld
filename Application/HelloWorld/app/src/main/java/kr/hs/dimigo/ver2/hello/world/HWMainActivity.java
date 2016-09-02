4fbb2f7f898074fe8346be55ebbc010bfdccc8aa
MR.LEE(상훈) <leeshoon1344@gmail.com>
2016-09-02 21:15:20 +0900

    Hello World Application > Create Project

---
+++ Application/HelloWorld/app/src/main/java/kr/hs/dimigo/ver2/hello/world/HWMainActivity.java
@@ -0,0 +1,13 @@
+package kr.hs.dimigo.ver2.hello.world;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HWMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hw_activity_main);
+    }
+}
