e61204cb627e59090c3817514884795f90b861c6
MR.LEE(상훈) <leeshoon1344@gmail.com>
2016-09-03 08:58:20 +0900

    Splited Application - Parsing (Developed by enoxaiming)

---
+++ Application-Parsing/app/src/main/java/kr/hs/dimigo/ver2/hello/world/CountryService.java
@@ -0,0 +1,19 @@
+package kr.hs.dimigo.ver2.hello.world;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+import static android.R.style.Widget;
+
+/**
+ * Created by JunHyeok on 2016. 9. 3..
+ */
+
+public interface CountryService {
+    @GET("json")
+    Call<RootClass> getCountry(@Query("latlng")String x,
+                               @Query("key") String key);
+}
+
---
+++ Application-Parsing/app/src/main/java/kr/hs/dimigo/ver2/hello/world/Viewport.java
@@ -0,0 +1,36 @@
+//
+//	Viewport.java
+//
+//	Create by 준혁 하 on 3/9/2016
+//	Copyright © 2016 준혁 하. All rights reserved.
+//	Model file Generated using: 
+//	Vin.Favara's JSONExportV https://github.com/vivi7/JSONExport 
+//	(forked from Ahmed-Ali's JSONExport)
+//
+package kr.hs.dimigo.ver2.hello.world;
+import android.location.Location;
+
+import org.json.*;
+import java.util.*;
+
+
+public class Viewport{
+
+	private Location northeast;
+	private Location southwest;
+
+	public void setNortheast(Location northeast){
+		this.northeast = northeast;
+	}
+	public Location getNortheast(){
+		return this.northeast;
+	}
+	public void setSouthwest(Location southwest){
+		this.southwest = southwest;
+	}
+	public Location getSouthwest(){
+		return this.southwest;
+	}
+
+
+}
